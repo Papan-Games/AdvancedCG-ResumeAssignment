bac18bd6364390579797e208c8014fa8def99719
DesmondYeoh-LeineX <desmondyeoh22@gmail.com>
2020-09-09 17:29:08 +0800

    Update v1

---
+++ Resume Assignment/Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour
+{
+    public Animator doorAnim;
+    public Collider DoorCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            doorAnim.SetBool("character_nearby", true);
+            DoorCollider.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            doorAnim.SetBool("character_nearby", false);
+            DoorCollider.enabled = true;
+        }
+    }
+}
---
+++ Resume Assignment/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/Player Move")]
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 5.0f;
+    public float gravity = 9.8f;
+    private bool canMove;
+
+    private CharacterController charController;
+    Vector3 moveDirection = Vector3.zero;
+
+   
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        canMove = true;
+    }
+
+    void Update()
+    {
+        // Movements
+        if (canMove)
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+
+            float deltaX = Input.GetAxis("Vertical") * speed;
+            float deltaY = Input.GetAxis("Horizontal") * speed;
+            float movementY = moveDirection.y;
+            moveDirection = (forward * deltaX) + (right * deltaY);
+
+            if (charController.isGrounded && Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            else
+            {
+                moveDirection.y = movementY;
+            }
+            if (!charController.isGrounded)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+
+            charController.Move(moveDirection * Time.deltaTime);
+        }
+    }
+}
