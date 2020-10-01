7ea696676083f9dea3fab52ff26b04eef512110b
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2020-10-01 19:27:11 +0800

    Did in-game menu script but not yet apply

---
+++ Resume Assignment/Assets/Peiyi/Script/InGameMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InGameMenuScript : MonoBehaviour
+{
+    public GameObject confirmPopup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirmPopup.SetActive(false);
+    }
+
+    public void OnClickMenu()
+    {
+        confirmPopup.SetActive(true);
+    }
+
+    public void OnClickYes()
+    {
+        confirmPopup.SetActive(false);
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OnClickNo()
+    {
+        confirmPopup.SetActive(true);
+    }
+
+}
