00f28b9d1264c3eae5e5a14205bfcb50de730784
peiyi456 <36634171+peiyi456@users.noreply.github.com>
2020-10-02 06:23:01 +0800

    change some comments in the scripts

--- Resume Assignment/Assets/Peiyi/Script/ObjectRotate.cs
+++ Resume Assignment/Assets/Peiyi/Script/ObjectRotate.cs
@@ -13,6 +13,6 @@ public class ObjectRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(50 * Time.deltaTime, 0, 0); //rotates 50 degrees per second around z axis
+        transform.Rotate(50 * Time.deltaTime, 0, 0); //rotates 50 degrees per second around x axis
     }
 }
