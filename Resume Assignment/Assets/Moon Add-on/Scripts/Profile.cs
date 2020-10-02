12055ec19ec01d2ccff121bfd3224c24d853834f
vivianng1998 <69830300+vivianng1998@users.noreply.github.com>
2020-10-02 19:42:55 +0800

    info profile trace wip

--- Resume Assignment/Assets/Moon Add-on/Scripts/Profile.cs
+++ Resume Assignment/Assets/Moon Add-on/Scripts/Profile.cs
@@ -17,21 +17,21 @@ public class Profile : MonoBehaviour
         {
             fullName.text = "Ng Chia Ling";
             contactNumber.text = "017-5165776";
-            email.text = "vivianng1998@1utar.my";
+            email.text = "vivianng1998 @ 1utar.my";
             address.text = "242, Lorong 6, Taman Kami A, 34000 Taiping, Perak";
         }
         else if (Manager.instance.PeiYi)
         {
             fullName.text = "Tham Pei Yi";
             contactNumber.text = "016-6919861";
-            email.text = "peiyi456@1utar.my";
+            email.text = "peiyi456 @ 1utar.my";
             address.text = "CG-013, Jalan Indah 3, Taman Universiti Indah, 43300 Seri Kembangan, Selongor";
         }
         else if (Manager.instance.KengLiang)
         {
             fullName.text = "Yeoh Keng Liang";
             contactNumber.text = "016-6505631";
-            email.text = "desmondyeoh22@1utar.my";
+            email.text = "desmondyeoh22 @ 1utar.my";
             address.text = "24, Jalan USJ 8/2E, 47610 Subang Jaya, Selangor";
         }
     }
